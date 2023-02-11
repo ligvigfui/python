1888b97814627fb1a3117d61b384407c2a872a0a
ligvigfui <ligvigfui@gmail.com>
2023-02-11 20:03:06 +0100

    security upgrade and login integration

--- neptun_fos/main.py
+++ neptun_fos/main.py
@@ -2,11 +2,70 @@ import datetime
 import pyautogui
 import json
 import os
+import socket
+import hash
 
-
-
-
+def create_tcp_connection(ip_address, port) -> socket.socket:
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
+    # Connect the socket to the specified address and port
+    client_socket.connect((ip_address, port))
+    
+    return client_socket
+
+def send_get_request(client_socket, host, credentials):
+    # Compose the GET request message
+    request = "GET /login_neptun_fos HTTP/1.1\r\n"
+    request += f"Host: {host}\r\n"
+    request += f"Credentials: {credentials}\r\n\r\n"
+
+    # Send the GET request message
+    client_socket.send(request.encode())
+
+def receive_response(client_socket) -> str:
+    # Receive the response header
+    header = ""
+    while not header.endswith("\r\n\r\n"):
+        header += client_socket.recv(1).decode()
+
+    # Extract the content length from the header
+    content_length = int(header.split("Content-Length: ")[1].split("\r\n")[0])
+
+    # Receive the response body
+    body = client_socket.recv(content_length).decode()
+
+    return header + body
+
+def login(credentials, host, port) -> bool:
+    
+    # Example usage
+    credentials1 = hash.encode(credentials)
+
+    # open client socket
+    client_socket = create_tcp_connection(host, port)
+
+    # Assume that client_socket is already connected
+    send_get_request(client_socket, host, credentials1)
+
+    # Example usage
+    response = receive_response(client_socket)
+    response = response.split("\r\n\r\n")[1]
+    stuff = hash.decode(response, data["credentials"])
+    if stuff == "Ok":
+        print("Login successful")
+        return False
+    elif stuff == "Wc":
+        print("Wrong credentials")
+    elif stuff == "Nh":
+        print("Server responded badly")
+    elif stuff == "Nr":
+        print("No response from server")
+    else:
+        print("Unknown error")
+    return True
+    
+
 
 
 # read cfg file
@@ -20,7 +79,7 @@ except Exception as e:
     
     # The data you want to write to the file
     data = {
-        "credentials": "username:password",
+        "credentials": "email@something.com:password123",
         "targetting": "auto",
         "x_position": 1240,
         "neptun_mode": "separate_windows",
@@ -45,8 +104,8 @@ except Exception as e:
 # time to start looping (hh:mm:ss)
 
 
-
-
+host = "127.0.0.1"
+port = 7878
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -136,13 +195,16 @@ while True:
             else:
                 input('Invalid action: ' + actiona + '\nPress enter to continue...')
     elif action == '2':
+        if login(data["credentials"], host, port):
+            action = ''
+            continue
         print('Neptun FOS/start neptun run')
         if data["start_method"] == 'at_time':
             # wait for start time
             print('Waiting for start time...')
             while True:
                 if datetime.datetime.now().strftime("%H:%M:%S") >= data["start_time"]:
-                    pyautogui.sleep(0.1)
+                    pyautogui.sleep(0.2)
                     break
         elif data["start_method"] == 'after_delay':
             # convert data["start_time"] to seconds
@@ -153,7 +215,7 @@ while True:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print(str(delay - second) + ' seconds left')
                 pyautogui.sleep(1)
-            pyautogui.sleep(0.1)
+            pyautogui.sleep(0.2)
         runing = True
         found_neptuns = 0
         if data["targetting"] == 'manual':
@@ -194,46 +256,3 @@ while True:
     else:
         input('Invalid input: ' + action + '\nPress enter to continue...')
         continue
-
-
-while False:
-    # settings
-
-    # take user input to a variable
-    delay = input('Enter the delay in seconds: ')
-    # convert the string to an integer
-    delay = int(delay)
-
-    # write code that waits 2 seconds
-    print('Waiting ' + str(delay) + ' seconds...')
-    pyautogui.sleep(delay)
-
-    print('Taking screenshot now!')
-    # Take a screenshot of the entire screen
-
-
-    runing = True
-    while runing:
-        steak = 0
-        found = False
-        screenshot = pyautogui.screenshot()
-        # Search for the color FFFFFF from top to bottom, starting from x=1250
-        for y in range(screenshot.size[1]):
-            if screenshot.getpixel((1240, y)) == (128, 128, 128):
-                steak = steak + 1
-            else:
-                steak = 0
-            if steak == 5:    
-                pyautogui.click(1240, y+5)
-                pyautogui.moveTo(10,10)
-                if data("neptun_mode") == 'separate_window':
-                    pyautogui.hotkey('ctrl', 'tab')
-                elif data("neptun_mode") == 'in_browser':
-                    pyautogui.hotkey('ctrl', 'win', 'right')
-                print('Found the color at y = ' + str(y+5))
-                found = True
-                break
-        print('Broke out of loop: found: ' + str(found))
-        runing = False
-        if found:
-            runing = True
