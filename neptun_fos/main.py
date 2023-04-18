07d2ba6c820ba07b367349deed0b38e279e09162
ligvigfui <ligvigfui@gmail.com>
2023-04-18 22:35:31 +0200

    bugfix

--- neptun_fos/main.py
+++ neptun_fos/main.py
@@ -79,7 +79,7 @@ def read_cfg() -> dict:
         
         # The data you want to write to the file
         data = {
-            "ip": "ligvigfui.ddns.net:7878",
+            "ip": "ligvigfui.ddns.net",
             "email": "email@something.com",
             "password": "password123",
             "targetting": "auto",
